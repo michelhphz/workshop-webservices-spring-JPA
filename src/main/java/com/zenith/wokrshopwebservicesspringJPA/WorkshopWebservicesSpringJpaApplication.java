9042973d43cd2bf441cb038dd73ae7ea7861bd6a
michelPech <michelhphz@hotmail.com>
2020-06-29 20:31:52 -0300

    Criacao do projeto

---
+++ src/main/java/com/zenith/wokrshopwebservicesspringJPA/WorkshopWebservicesSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.zenith.wokrshopwebservicesspringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopWebservicesSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopWebservicesSpringJpaApplication.class, args);
+	}
+
+}
