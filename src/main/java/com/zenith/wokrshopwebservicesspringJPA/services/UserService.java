1a2bfe77d4351ab97df097dc4ef4978474b16695
michelPech <michelhphz@hotmail.com>
2020-06-30 20:28:02 -0300

    Camada de servico, registro de componentes

---
+++ src/main/java/com/zenith/wokrshopwebservicesspringJPA/services/UserService.java
@@ -0,0 +1,26 @@
+package com.zenith.wokrshopwebservicesspringJPA.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zenith.wokrshopwebservicesspringJPA.entities.User;
+import com.zenith.wokrshopwebservicesspringJPA.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
