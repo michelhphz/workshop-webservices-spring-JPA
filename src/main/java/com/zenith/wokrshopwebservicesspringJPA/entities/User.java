7a1d6012845371d3178bc92be77cf1121199cea7
michelPech <michelhphz@hotmail.com>
2020-06-30 21:40:13 -0300

    Entidade Pedido. Datas com Instant e padrao ISO 8601

--- src/main/java/com/zenith/wokrshopwebservicesspringJPA/entities/User.java
+++ src/main/java/com/zenith/wokrshopwebservicesspringJPA/entities/User.java
@@ -1,11 +1,14 @@
 package com.zenith.wokrshopwebservicesspringJPA.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User implements Serializable{
@@ -19,6 +22,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -67,6 +73,10 @@ public class User implements Serializable{
 	public Long getId() {
 		return id;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}	
 
 	@Override
 	public int hashCode() {
@@ -92,4 +102,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
 }
